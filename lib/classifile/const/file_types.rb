6cebe9b70edc14130c85aa9d3ae01a4a7b3bd124
kurehajime <xiidec@gmail.com>
2021-09-14 14:36:33 +0900

    fix extension checker

--- lib/classifile/const/file_types.rb
+++ lib/classifile/const/file_types.rb
@@ -4,6 +4,6 @@
 # extension
 module Classifile
   FILE_TYPE_IMAGE = %w[.gif .jpg .jpeg .jpe .jfif .png .bmp .dib .rle .ico .ai .art .psd .tif .tiff .nsk .webp].freeze
-  FILE_TYPE_SOUND = %w[.mp3 .wma .asf .3gp .3g2 .aac .ogg .oga .mov .m4a .alac .flac .wav].freeze
-  FILE_TYPE_MOVIE = %w[.avi .flv .mpg .mpeg .mp4 .mkv .mov .qt .wmv .asf .m2ts .ts .m4a .webm .ogm].freeze
+  FILE_TYPE_SOUND = %w[.mp3 .wma .3gp .3g2 .aac .ogg .oga .m4a .alac .flac .wav].freeze
+  FILE_TYPE_MOVIE = %w[.avi .flv .mpg .mpeg .mp4 .mkv .mov .qt .wmv .asf .m2ts .ts .webm .ogm].freeze
 end
--- test/checker/extension_checker_test.rb
+++ test/checker/extension_checker_test.rb
@@ -7,15 +7,46 @@ class ExtensionCheckerTest < Minitest::Test
     include Classifile::ExtensionChecker
   end
 
-  def test_check_extension
-    checker = Checker.new
-    checker.extname = ".jpg"
-    checker.image?
-    assert_raises Classifile::Failed do
-      checker.sound?
+  def setup
+    @checker = Checker.new
+  end
+
+  def test_check_image
+    Classifile::FILE_TYPE_IMAGE.each do |ext|
+      @checker.extname = ext
+      @checker.image?
+      assert_raises Classifile::Failed do
+        @checker.sound?
+      end
+      assert_raises Classifile::Failed do
+        @checker.movie?
+      end
     end
-    assert_raises Classifile::Failed do
-      checker.movie?
+  end
+
+  def test_check_sound
+    Classifile::FILE_TYPE_SOUND.each do |ext|
+      @checker.extname = ext
+      @checker.sound?
+      assert_raises Classifile::Failed do
+        @checker.image?
+      end
+      assert_raises Classifile::Failed do
+        @checker.movie?
+      end
+    end
+  end
+
+  def test_check_movie
+    Classifile::FILE_TYPE_MOVIE.each do |ext|
+      @checker.extname = ext
+      @checker.movie?
+      assert_raises Classifile::Failed do
+        @checker.image?
+      end
+      assert_raises Classifile::Failed do
+        @checker.sound?
+      end
     end
   end
 end
